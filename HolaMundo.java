136b7ef6528fbca50d6d752e25498ae365991587
David_de_Toro <114107350+DavidDeToro@users.noreply.github.com>
2023-09-20 19:48:40 +0200

    Update HolaMundo.java

--- HolaMundo.java
+++ HolaMundo.java
@@ -1,5 +1,12 @@
 public class HolaMundo {
     public static void main(String[] args) {
         System.out.println("Hola Mundo");
+        int edad = 18;
+
+        if (edad < 18){
+            System.out.println("Es menor de edad");
+        }else {
+            System.out.println("Puede conducir");
+        }
     }
 }
